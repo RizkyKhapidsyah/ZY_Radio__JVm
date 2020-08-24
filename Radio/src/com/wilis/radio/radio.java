026f75e3a2ab13f73bbeccec78bd93815e1647a1
RizkyKhapidsyah <RizkyKhapidsyah@RIKY_PC>
2020-08-24 09:52:32 +0700

    Initial commit

---
+++ Radio/src/com/wilis/radio/radio.java
@@ -0,0 +1,13 @@
+package com.wilis.radio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class radio extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
